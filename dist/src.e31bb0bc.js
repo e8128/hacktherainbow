f6e751a491df3ed782c918a8d658b8c400c52a41
futurewitness <tleong073@gmail.com>
2020-09-29 01:09:14 -0400

    trying to fix

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -22990,7 +22990,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54828" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54968" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
